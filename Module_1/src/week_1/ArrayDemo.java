e35c8e01ce9b839a7c2d12959a998612dadec7e5
Admin <Admin@DESKTOP-28SN13C>
2023-11-08 01:08:36 +0530

    Added class Two dimensional aray

--- Module_1/src/week_1/ArrayDemo.java
+++ Module_1/src/week_1/ArrayDemo.java
@@ -8,6 +8,7 @@ public class ArrayDemo {
 	int mark[]= {10,20,30,40}; // single dimensional array
 	
 	int array[]=new int[5]; //array with 1 ROW and 5 COLUMNS initiated
+							//Array can't be resized
 	
 	void arr() {
 		array[0]=80; //values have been set to the array indexes 
---
+++ Module_1/src/week_1/TwoDimensionalArray.java
@@ -0,0 +1,38 @@
+package week_1;
+
+public class TwoDimensionalArray {
+		int Array2D_v1[][]= {  //two dimensional array with 2 rows and 4 columns
+				{10,20,30,40},
+				{90,80,70,60},
+		};
+		
+		int Array2D_V2[][]=new int[2][4];
+		
+		void array2d_v1() {
+			System.out.println("Values in 2d array v1 : ");
+			
+			for(int i=0;i<Array2D_v1.length;i++) {			// This loops around 4 times
+				for(int k=0;k<Array2D_v1[i].length;k++) {
+					System.out.print(Array2D_v1[i][k]+" ");  //This loops around 4*4 = 16 times 
+				}
+				System.out.println();
+			}
+		}
+		
+		void array2d_v2() {
+			System.out.println("Values in 2d array v2 : ");
+			
+			for(int i=0;i<Array2D_V2.length;i++) {
+				for(int k=0;k<Array2D_V2[i].length;i++) {
+					System.out.print(Array2D_V2[i][k]+" ");
+				}
+				System.out.println();
+			}
+		}
+		
+		public static void main(String[] args) {
+			TwoDimensionalArray ArrayTypes =new TwoDimensionalArray();
+			ArrayTypes.array2d_v1();
+			ArrayTypes.array2d_v2();
+		}
+}
