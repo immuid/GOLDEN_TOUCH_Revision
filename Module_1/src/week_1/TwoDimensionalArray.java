4204d21b6968005eb0439219e4ee2b139be91cb2
imranahmedm <imranahmedm@FSSRENTAL124.fss.india>
2023-11-08 12:48:10 +0530

    solved the error in 2Dimensiona_Array

--- Module_1/src/week_1/TwoDimensionalArray.java
+++ Module_1/src/week_1/TwoDimensionalArray.java
@@ -9,21 +9,21 @@ public class TwoDimensionalArray {
 		int Array2D_V2[][]=new int[2][4];
 		
 		void array2d_v1() {
-			System.out.println("Values in 2d array v1 : ");
+			System.out.println("Values in 2d array v1 : \n");
 			
 			for(int i=0;i<Array2D_v1.length;i++) {			// This loops around 4 times
-				for(int k=0;k<Array2D_v1[i].length;k++) {
-					System.out.print(Array2D_v1[i][k]+" ");  //This loops around 4*4 = 16 times 
+				for(int k=0;k<Array2D_v1[i].length;k++) {  //This loops around 4*4 = 16 times 
+					System.out.print(Array2D_v1[i][k]+" "); 
 				}
 				System.out.println();
 			}
 		}
 		
 		void array2d_v2() {
-			System.out.println("Values in 2d array v2 : ");
+			System.out.println("\nValues in 2d array v2 : \n ");
 			
 			for(int i=0;i<Array2D_V2.length;i++) {
-				for(int k=0;k<Array2D_V2[i].length;i++) {
+				for(int k=0;k<Array2D_V2[i].length;k++) {
 					System.out.print(Array2D_V2[i][k]+" ");
 				}
 				System.out.println();
