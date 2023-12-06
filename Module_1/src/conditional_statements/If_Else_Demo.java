2526485d7ca715a94b2fa41048b4ce71b2f5da3e
Admin <Admin@DESKTOP-28SN13C>
2023-12-06 22:35:50 +0530

    Conditional statements updated ...

--- Module_1/src/conditional_statements/If_Else_Demo.java
+++ Module_1/src/conditional_statements/If_Else_Demo.java
@@ -6,6 +6,8 @@ public class If_Else_Demo {
 		int j= 16;
 		int k= 10;
 		int l= 22;
+		boolean valid = true;
+		String s="hello";
 		
 		If_Else_Demo If_Else_Demo=new If_Else_Demo();
 		If_Else_Demo.ifElseDemo(i);
@@ -15,17 +17,21 @@ public class If_Else_Demo {
 		If_Else_Demo.ifElseORDemo2(i);
 		If_Else_Demo.ifElseANDDemo3(l);
 		
+		If_Else_Demo.ifBooleanDemo(valid);
+		If_Else_Demo.StringDemo(s);
+		
 	}
 		
 		
 	String ifElseDemo(int i) {
 		String result;
+		System.out.println("This method checks the i value is to 15 or not ...");
 		if(i==15) {
-			result=" Value of i is equal to 15 ... ";
+			result=" Value of i is equal to 15 ... \n";
 			System.out.println(result);
 		}
 		else {
-			result=" Value of i is not equal to 15 ... ";
+			result=" Value of i is not equal to 15 ... \n";
 			System.out.println(result);
 		}		
 		return result;
@@ -33,25 +39,31 @@ public class If_Else_Demo {
 	
 	String ifElseDemo1(int i) {
 		String result = null;
+		System.out.println("This method checks whether i value is lesser than 20 or erual to 10 or greater than 20 ...");
+		
 		if(i<20) {
-			result=" Value of i is lesser than 20 ... ";
+			result=" Value of i is lesser than 20 ... \n";
 			System.out.println(result);
 		}
 		else if(i==10) {  // this condition is not executed when the first condition is satisfied ...
-			result=" Value of i is not equal to 10 ... ";
+			result=" Value of i is not equal to 10 ... \n";
 			System.out.println(result);
-		}		
+		}else {
+			result="Value of i is greater than 20 ... \n";
+			System.out.println(result);
+		}
 		return result;
 	}
 	
 	String ifElseORDemo2(int i) {
 		String result = null;
+		System.out.println("This method used to represent OR condition ...");
 		if(i<10 || i>20) {
-			result=" Value of i is lesser than 10 ... or greater than 20 ... ";
+			result=" Value of i is lesser than 10 ... or greater than 20 ... \n";
 			System.out.println(result);
 		}
 		else {  // this condition is not executed when the first condition is satisfied ...
-			result=" Value of i is greater than 10 ... and lesser than 20 ";
+			result=" Value of i is greater than 10 ... and lesser than 20 ... \n";
 			System.out.println(result);
 		}		
 		return result;
@@ -59,14 +71,39 @@ public class If_Else_Demo {
 	
 	String ifElseANDDemo3(int i) {
 		String result = null;
+		System.out.println("This method used to represent AND condition ...");
 		if(i%2==0 && i>20) {
-			result=" Value of i is greater than 20 ... and divisible by 2 ";
+			result=" Value of i is greater than 20 ... and divisible by 2 \n";
 			System.out.println(result);
 		}
 		else {  // this condition is not executed when the first condition is satisfied ...
-			result=" Value of i is either lesser than 20 ... or divisible by 2 ";
+			result=" Value of i is either lesser than 20 ... or divisible by 2 \n";
 			System.out.println(result);
 		}		
 		return result;
+	}
+	
+	String ifBooleanDemo(boolean value) {
+		String result;
+		System.out.println("This method used to represent boolean condition ...");
+		if(value) {
+			result="The boolean is valid ... \n";
+		System.out.println(result);
+		}else {
+			result="The boolean is not valid ... \n";
+		}
+		return result;
+	}
+	
+	void StringDemo(String s) {
+		System.out.println("This method is used to represent String demo ...");
+		if(s.equalsIgnoreCase("Hello")) {
+			System.out.println("The value of s is Hello \n");
+		}
+		else if(s.equals("hello")) {
+			System.out.println("The value of s is Hello \n");
+		}else {
+			System.out.println("The value not matched \n");
+		}
 	}
 }
